befff91eedd37cf6a511002d271d265735eac3a2
explore11 <prefect_start@163.com>
2020-09-16 23:32:35 +0800

    增加课程列表显示和分页接口

---
+++ api/course.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+
+export default {
+  getPageListByFront(currentPage, limit, queryCourse) {
+    return request({
+      url: `/eduService/eduCourse/getPageListByFront/${currentPage}/${limit}`,
+      method: 'post',
+      data: queryCourse
+    })
+  },
+  // 获取课程二级分类
+  getAllSubject() {
+    return request({
+      url: `/eduService/eduSubject/getAllSubjectByOneAndTwo`,
+      method: 'get'
+    })
+  }
+}
